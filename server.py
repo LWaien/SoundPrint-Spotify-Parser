55157d36d1ed53e7f69cff6d7292368ae8159022
LWaien <lawaien14@gmail.com>
2023-06-20 10:56:25 -0400

    get tracks functionality added

--- server.py
+++ server.py
@@ -28,18 +28,49 @@ def formdisplay():
     }
     response = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=urlencode(data))
     token_data = response.json()
-
     # Use the access token to make API requests on behalf of the user
     access_token = token_data.get('access_token')
-
     # Example API request - fetch user profile
     profile_response = requests.get('https://api.spotify.com/v1/me', headers={'Authorization': 'Bearer ' + access_token})
     profile_data = profile_response.json()
+
 
     # Example API request - fetch user playlists
     playlists_response = requests.get('https://api.spotify.com/v1/me/top/artists', headers={'Authorization': 'Bearer ' + access_token})
     playlists_data = playlists_response.json()
     #print(playlists_data['items'])
+
+
+    limit = 50
+    offset = 0
+
+    while True:
+        # Send a GET request to the endpoint with the limit and offset
+        params = {
+            'market':'US',
+            'limit': limit,
+            'offset': offset
+        }
+        response = requests.get('https://api.spotify.com/v1/me/tracks', headers={'Authorization': 'Bearer ' + access_token}, params=params)
+        data = response.json()
+
+        # Iterate over each track and display the track name and artists
+        for item in data['items']:
+            track = item['track']
+            track_name = track['name']
+            artists = [artist['name'] for artist in track['artists']]
+
+            print(f"Track: {track_name}")
+            print(f"Artists: {artists}")
+            print()
+
+        # Increment the offset by the limit to get the next page
+        offset += limit
+
+        # If there are no more items, exit the loop
+        if len(data['items']) < limit:
+            break
+
     topartists = []
     for i in playlists_data['items']:
         ### use a shorter time frame for top artists to get things the user might be more into lately and hasnt seen yet
@@ -47,7 +78,7 @@ def formdisplay():
         ###whatever. ALSO add parameters for length of top artists (time back) and do data scraping basing on all library songs etc. You should also do 
         ### something with the recommendation section of the api. Top songs could also be a good indicator for concert
         topartists.append([i['name'],i['popularity'],i['id']])
-        print(i['name']," - ",i['popularity']," - ",i['id'])
+        #print(i['name']," - ",i['popularity']," - ",i['id'])
     #print(playlists_response)
     return playlists_data
 
