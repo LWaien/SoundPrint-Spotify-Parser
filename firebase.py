76f91ec0436f72831c5228351c5197781adbae5c
LWaien <lawaien14@gmail.com>
2023-07-09 11:23:44 -0400

    working db insert

--- firebase.py
+++ firebase.py
@@ -9,15 +9,9 @@ import json
 #print(users.child('OthrName').get())
 
 def insertUser(email,fname,lname,maxdist,location):
-    print('db')
     cred = credentials.Certificate("credentials.json")
-    print('cred')
     firebase_admin.initialize_app(cred, {'databaseURL':'https://concertrec-da7fc-default-rtdb.firebaseio.com/'})
-    print('init')
     ref = db.reference('/py')
-    print('ref')
     users = ref.child('users')
-    print('ref2')
-    users.child(email).set({'spotify_user':None,'fname':fname,'lname':lname,'maxdist':maxdist,'location':location,
-                            'last_email':None,'top_artists':None, 'lib_artists':None})
-    print('added to db')
+    users.push({'email':email,'spotify_user':None,'fname':fname,'lname':lname,'maxdist':maxdist,'location':location,
+                           'last_email':None,'top_artists':None, 'lib_artists':None})
