ce9069cceaa3ae3e261f8e5c1af29cdc0ebef037
LWaien <lawaien14@gmail.com>
2023-09-21 14:25:47 -0400

    removing redis

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from flask import Flask, redirect, url_for, request,jsonify, make_response
 import collectdata
 import fb
 import os
-from rq import Queue
-from redis import Redis
+
 
 
 CLIENT_ID = "32f3ca3f815c4b7f91335ffeb5d90f7d"
@@ -19,33 +18,33 @@ print('spotify loader is running...')
 
 @app.route("/generateData/<spotify_user>/<access_token>",methods=['GET'])
 def generateData(spotify_user, access_token):
-    print("loading spotify data")
-    result = queue.enqueue(scanSpotify, spotify_user, access_token)
-    return make_response({'msg':'adding data','status':200})
-
-def scanSpotify(spotify_user,access_token):
-        #route that accepts spotify user's access token. Endpoint then collects data to be saved for recommendations in the future
+    #route that accepts spotify user's access token. Endpoint then collects data to be saved for recommendations in the future
     print("Generating data")
     try:
-        libdata = collectdata.gatherLibData(access_token)
+        #libdata = collectdata.gatherLibData(access_token)
+        libdata = queue.enqueue(collectdata.gatherLibData, access_token)
     except:
         print("Unable to load libdata")
         libdata = None
 
     try:
-        topartists = collectdata.gatherTopArtists(access_token)
+        #topartists = collectdata.gatherTopArtists(access_token)
+        topartists = queue.enqueue(collectdata.gatherTopArtists, access_token)
     except:
         print("Unable to load topartists")
         topartists = None
 
     try:
-        topsongs = collectdata.gatherTopSongs(access_token)
+        #topsongs = collectdata.gatherTopSongs(access_token)
+        topsongs = queue.enqueue(collectdata.gatherTopSongs, access_token)
     except:
         print("Unable to load topsongs")
         topsongs = None
 
         #add spotify username to this function
     msg,code = fb.addSpotifyData(spotify_user,topartists.result,libdata.result,topsongs.result)
+
+    return make_response({'msg':msg,'status':code})
 
 
 if __name__ == "__main__":
