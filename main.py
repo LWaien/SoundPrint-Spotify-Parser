eb648749fe3215dcfb0ce254e744787e0a70607e
LWaien <lawaien14@gmail.com>
2023-09-21 14:26:59 -0400

    removing redis

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from flask import Flask, redirect, url_for, request,jsonify, make_response
 import collectdata
 import fb
-import os
 
 
 
 CLIENT_ID = "32f3ca3f815c4b7f91335ffeb5d90f7d"
 CLIENT_SECRET = "3f882c04f6824a68b45b251ff922488a"
 
-
-redis_conn = Redis.from_url(os.getenv('REDIS_URL'))
-queue = Queue('my_queue', connection=redis_conn)
 
 app = Flask(__name__)
 
@@ -21,28 +17,25 @@ def generateData(spotify_user, access_token):
     #route that accepts spotify user's access token. Endpoint then collects data to be saved for recommendations in the future
     print("Generating data")
     try:
-        #libdata = collectdata.gatherLibData(access_token)
-        libdata = queue.enqueue(collectdata.gatherLibData, access_token)
+        libdata = collectdata.gatherLibData(access_token)
     except:
         print("Unable to load libdata")
         libdata = None
 
     try:
-        #topartists = collectdata.gatherTopArtists(access_token)
-        topartists = queue.enqueue(collectdata.gatherTopArtists, access_token)
+        topartists = collectdata.gatherTopArtists(access_token)
     except:
         print("Unable to load topartists")
         topartists = None
 
     try:
-        #topsongs = collectdata.gatherTopSongs(access_token)
-        topsongs = queue.enqueue(collectdata.gatherTopSongs, access_token)
+        topsongs = collectdata.gatherTopSongs(access_token)
     except:
         print("Unable to load topsongs")
         topsongs = None
 
         #add spotify username to this function
-    msg,code = fb.addSpotifyData(spotify_user,topartists.result,libdata.result,topsongs.result)
+    msg,code = fb.addSpotifyData(spotify_user,topartists,libdata,topsongs)
 
     return make_response({'msg':msg,'status':code})
 
