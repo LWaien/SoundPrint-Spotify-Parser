bf8a9009e43d565c103cf6037d241553bd9dd42e
LWaien <lawaien14@gmail.com>
2023-09-21 12:52:04 -0400

    added logging for generating data function

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ CLIENT_SECRET = "3f882c04f6824a68b45b251ff922488a"
 
 app = Flask(__name__)
 
-print('everything is running')
+print('spotify loader is running...')
 
 @app.route("/generateData/<spotify_user>/<access_token>",methods=['GET'])
 def generateData(spotify_user, access_token):
     #route that accepts spotify user's access token. Endpoint then collects data to be saved for recommendations in the future
-    print("genData")
+    print("Generating data")
 
     libdata = collectdata.gatherLibData(access_token)
     topartists = collectdata.gatherTopArtists(access_token)
